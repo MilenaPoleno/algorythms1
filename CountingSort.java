21e0b086d802cba289a9e2e514503f4734d30c9e
MilenaPoleno <112932828+MilenaPoleno@users.noreply.github.com>
2023-05-23 15:38:39 +0300

    Update CountingSort.java

--- CountingSort.java
+++ CountingSort.java
@@ -20,3 +20,34 @@ public class CountingSort {
         }
     }
 }
+
+
+// вторая реализация - вывод в строку
+
+public class CountingSort {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 3, 2, 3, 4, 2, 1, 1, 2, 
+            3, 4, 4, 2, 3, 1, 4, 2, 3, 4, 2, 1, 2, 3, 4, 
+            2, 1, 3, 4, 2, 1, 2, 3, 4, 4, 2, 3, 3, 2, 4};
+        int k = 50;
+        countingSort(array, k);
+    }
+    
+    static void countingSort(int[] array, int k) {
+        int[] count = new int[k + 1];
+        for (int number : array) {
+            count[number]++;
+            
+        }
+        int z = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                array[z] = i;
+                z++;
+                count[i]--;
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
+
