3334b294fffe98a8ef55a05dfd2e954b771a660b
MilenaPoleno <112932828+MilenaPoleno@users.noreply.github.com>
2023-05-23 15:22:22 +0300

    Update CountingSort.java

--- CountingSort.java
+++ CountingSort.java
@@ -14,7 +14,6 @@ public class CountingSort {
         for (int number : array) {
             count[number]++;            
         }
-        int z = 0;
         for (int i = 0; i < count.length; i++) {
             if (count[i] != 0) {
                 System.out.println(i + " -> " + count[i]);
